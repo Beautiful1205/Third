4fef71a072596430e6f15da348193c0190a31da2
houxuebo <houuxebo001@ke.com>
2019-04-20 18:18:27 +0800

    second commit

--- demo1/src/main/java/com/example/demo1/DemoThirdApplication.java
+++ demo1/src/main/java/com/example/demo1/DemoThirdApplication.java
@@ -1,12 +1,16 @@
 package com.example.demo1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoThirdApplication {
+    private static Logger LOGGER = LoggerFactory.getLogger(DemoThirdApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoThirdApplication.class, args);
+        LOGGER.info("应用启动成功...");
     }
 }
---
+++ demo1/src/main/java/com/example/demo1/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.example.demo1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author houxuebo on 2019-04-20 16:15
+ **/
+@RestController
+@RequestMapping("/say")
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(){
+        return "Hello!";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/bye", method = RequestMethod.GET)
+    public String bye(){
+        return "Bye!";
+    }
+
+
+}
